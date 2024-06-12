bf3646e60130b1ff6c6b76e961d2997b92cb1d1d
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-06-12 22:45:19 +0200

    Projects are now correctly dislayed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,11 +5,19 @@ export default {
   },
   data() {
     return {
+      projects:[]
     }
   },
   methods:{
     fetchPost(){
-      axios.get('http://127.0.0.1:8000/api/projects')
+      axios.get('http://127.0.0.1:8000/api/projects',{
+        params:{
+          page: 1
+        }
+      })
+      .then(res => {
+        this.projects = res.data.results.data
+      });
     }
   },
   created(){
@@ -27,6 +35,18 @@ export default {
         <h2 class="text-center ">
           I miei progetti
         </h2>
+      </div>
+      <div class="container">
+        <ul class="list-unstyled">
+          <li v-for="project in projects" :key="project.id" class="my-4">
+            <div class="my-1 d-flex justify-content-between">
+              <span class="fw-bolder">{{ project.title }}</span>
+              <span>{{ project.technology }}</span>
+              <span>{{ project.starting_date }}</span>
+            </div>
+            <div>{{ project.description }}</div>
+          </li>
+        </ul>
       </div>
     </section>
   </main>
