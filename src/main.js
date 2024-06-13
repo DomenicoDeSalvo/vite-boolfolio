874d467462e33fe603133d3b36ceff78a65c3202
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-06-13 18:47:45 +0200

    Added router and route for home

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
 import { createApp } from 'vue'
-
+import { createRoute, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import AppHome from './pages/AppHome.vue'
+
+
+const routes = [{path: '/', component: AppHome}]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+})
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-
-
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* add icons to the library */
-library.add(fas, far, fab)
+library.add()
 
 const app = createApp(App)
 
