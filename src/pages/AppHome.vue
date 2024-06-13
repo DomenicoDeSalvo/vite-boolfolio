37b8c920ff2ba217b3e18be7c0796df540f680cc
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-06-13 20:32:13 +0200

    Fix style AppHome

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,8 +1,12 @@
 <template>
-    <h1>Benvenuti</h1>
-    <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium eligendi impedit quidem sit voluptates necessitatibus sed quae enim vel ab, laborum reprehenderit perferendis maxime molestiae natus nam, voluptas numquam, labore provident neque omnis. Eius distinctio officiis sunt delectus expedita odio inventore obcaecati, et non veritatis, porro, unde sint tenetur voluptatum? Iure asperiores officiis dolor placeat ab, incidunt provident. Assumenda illum cum dolore sit suscipit nulla aliquid cupiditate aut, deserunt error modi commodi laboriosam nobis nostrum voluptate facere, harum eos dicta vel ipsam sed? Omnis inventore facilis nobis atque cupiditate odit accusamus porro officiis debitis enim dolore nostrum in dignissimos quisquam accusantium, deserunt, neque minima eaque doloremque, iste officia eligendi temporibus? Sed hic quis voluptates, autem ratione aut repellendus a enim atque quam tempora minima, asperiores labore fugit voluptas voluptatem quidem! Eveniet nihil incidunt impedit deleniti sit reiciendis, facere nisi quisquam expedita non aut quos et odio provident cum, at dolor unde itaque ipsam tempora nulla magni. Fugiat alias ducimus perspiciatis minima dolor! Vel, doloribus voluptatem eum quis aliquam, qui ea maiores optio dicta commodi sed. Eos ipsa ut omnis assumenda blanditiis hic, ad aperiam voluptatem id sunt sint, cumque debitis officiis alias? Dolores adipisci illum quaerat eligendi hic, autem velit!
-    </p>
+    <section>
+        <div class="container">
+            <h1 class="text-center">Benvenuti</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium eligendi impedit quidem sit voluptates necessitatibus sed quae enim vel ab, laborum reprehenderit perferendis maxime molestiae natus nam, voluptas numquam, labore provident neque omnis. Eius distinctio officiis sunt delectus expedita odio inventore obcaecati, et non veritatis, porro, unde sint tenetur voluptatum? Iure asperiores officiis dolor placeat ab, incidunt provident. Assumenda illum cum dolore sit suscipit nulla aliquid cupiditate aut, deserunt error modi commodi laboriosam nobis nostrum voluptate facere, harum eos dicta vel ipsam sed? Omnis inventore facilis nobis atque cupiditate odit accusamus porro officiis debitis enim dolore nostrum in dignissimos quisquam accusantium, deserunt, neque minima eaque doloremque, iste officia eligendi temporibus? Sed hic quis voluptates, autem ratione aut repellendus a enim atque quam tempora minima, asperiores labore fugit voluptas voluptatem quidem! Eveniet nihil incidunt impedit deleniti sit reiciendis, facere nisi quisquam expedita non aut quos et odio provident cum, at dolor unde itaque ipsam tempora nulla magni. Fugiat alias ducimus perspiciatis minima dolor! Vel, doloribus voluptatem eum quis aliquam, qui ea maiores optio dicta commodi sed. Eos ipsa ut omnis assumenda blanditiis hic, ad aperiam voluptatem id sunt sint, cumque debitis officiis alias? Dolores adipisci illum quaerat eligendi hic, autem velit!
+            </p>
+        </div>
+    </section>
 </template>
 
 <script>
