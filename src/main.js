dcf23cf5e1e39665b68ed1dd04c643897150eff3
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-06-13 19:50:17 +0200

    Fixes

--- src/main.js
+++ src/main.js
@@ -1,26 +1,17 @@
 import { createApp } from 'vue'
-import { createRoute, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import AppHome from './pages/AppHome.vue'
+import AppPortfolio from './pages/AppPortfolio.vue'
 
-
-const routes = [{path: '/', component: AppHome}]
+const routes = [
+    {path: '/', component: AppHome},
+    {path: '/portfolio', component: AppPortfolio},
+]
 
 const router = createRouter({
     history: createWebHistory(),
     routes: routes
 })
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* add icons to the library */
-library.add()
-
-const app = createApp(App)
-
-app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(router)
-app.mount('#app')
+createApp(App).use(router).mount('#app')
